88c3d6023a5a2da604c3257ae1036c70c8d430a1
Simon Li <orpheus+devel@gmail.com>
2020-01-18 01:10:19 +0000

    5.6.1

--- build.sh
+++ build.sh
@@ -14,7 +14,3 @@ echo "$PKG_NAME $PKG_VERSION $PKG_BUILDNUM" > "${OMERODIR}/etc/grid/.conda-build
 mkdir -p $PREFIX/bin
 cp $RECIPE_DIR/omero-web $PREFIX/bin/
 chmod +x $PREFIX/bin/omero-web
-
-# FIXME: OMERO.web templates are in the wrong place
-mkdir -p "${PREFIX}/opt/omero/web/OMERO.web/etc/templates"
-mv "${SP_DIR}/omeroweb/templates" "${OMERODIR}/etc/templates/web"
