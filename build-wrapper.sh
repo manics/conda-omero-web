f4acb0c32b5782aa6a7e1003c9e8968f21f2a9ba
Simon Li <orpheus+devel@gmail.com>
2020-01-18 22:26:08 +0000

    Move omero-web wrapper into seperate package

--- build-wrapper.sh
+++ build-wrapper.sh
@@ -2,8 +2,6 @@
 
 set -eux
 
-python -m pip install . --no-deps --ignore-installed -vv
-
 OMERODIR="${PREFIX}/opt/omero/web/OMERO.web"
 
 mkdir -p "${OMERODIR}/etc/grid/"
@@ -12,5 +10,6 @@ mkdir -p "${OMERODIR}/etc/grid/"
 echo "$PKG_NAME $PKG_VERSION $PKG_BUILDNUM" > "${OMERODIR}/etc/grid/.conda-build"
 
 mkdir -p $PREFIX/bin
-cp $RECIPE_DIR/omero-web $PREFIX/bin/
-chmod +x $PREFIX/bin/omero-web
+cp $RECIPE_DIR/omero-web* $PREFIX/bin/
+
+chmod +x $PREFIX/bin/omero-web*
